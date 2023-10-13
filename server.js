2f3488db032b11c599f7806961a24b0d1e285ae5
bhargavnaidu77 <120448254+bhargavnaidu77@users.noreply.github.com>
2023-10-13 15:23:22 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+    cors: {
+      origin: '*',
+    }
+  });
+app.use(cors());
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('message', (message) => {
+    io.emit('message', message); // Broadcast the message to all connected clients
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+server.listen(4000, () => {
+  console.log('Server is running on http://localhost:4000');
+});
